70ee2f66813435124cf8cbec27f36b2f870ad294
James Piñas <james.pinas@gmail.com>
2024-05-30 09:50:50 -0500

    avance

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,13 @@ public class Main {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new vistaCompra().setVisible(true);
+                //new vistaCompra().setVisible(true);
                 new vistaPedido().setVisible(true);
-                new vistaRegistrarCliente().setVisible(true);
-                new vistaBuscarCliente().setVisible(true);
-                new vistaContacto().setVisible(true);
-            }
+            //     new vistaRegistrarCliente().setVisible(true);
+            //     new vistaBuscarCliente().setVisible(true);
+            //     new vistaContacto().setVisible(true);
+            // 
+        }
         });
     }
 }
