a35e7c0a5bd0ca6652a5ecb00077b19a4d494506
James Piñas <james.pinas@gmail.com>
2024-07-01 23:27:11 -0500

    avance
    se agrego la clase fuentes

---
+++ src/Vista/Fuentes.java
@@ -0,0 +1,31 @@
+package Vista;
+
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.io.File;
+import java.io.IOException;
+
+public class Fuentes {
+
+
+    private static Font playwriteFont;
+    private static Font robotoFont;
+
+    static {
+        try {
+            playwriteFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/Fuentes/PLAYWRITEITMODERNA-REGULAR.ttf")).deriveFont(18f);
+            robotoFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/Fuentes/Roboto-Regular.ttf")).deriveFont(18f);
+        } catch (FontFormatException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Font getPlaywriteFont(float size, int style) {
+        return playwriteFont.deriveFont(style, size);
+    }
+
+    public static Font getRobotoFont(float size, int style) {
+        return robotoFont.deriveFont(style, size);
+    }
+    
+}
--- src/Vista/Interfaz/VistaLogin.java
+++ src/Vista/Interfaz/VistaLogin.java
@@ -6,32 +6,47 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
 
 import Controlador.*;
 import Vista.ImagenFondo;
-
+import Vista.Fuentes;
 
 public class VistaLogin extends JFrame {
 
     private DefaultTableModel model;
     private JTable table;
     private JScrollPane scrollPane;
+    private Font playwriteFont;
+    private Font robotoFont;
+    private Font aptosblack;
 
     private JTextField iDUsuarioField;
-    private JTextField contrasenaField;
-    
+    private JPasswordField contrasenaField;
+
     private JButton btnIngresar;
     private JButton btnSalir;
     private JButton btnContacto;
-
+    private JButton btnSalir2;
 
     public VistaLogin() {
-        this.setTitle("EMPRESA SOCOSANI:   Login");
-        this.setFont(new Font("Aptos Black", Font.BOLD, 35));
-        this.setBounds(0, 0, 550, 350);
+        this.setTitle("EMPRESA SOCOSANI: Login");
+        this.setFont(Fuentes.getRobotoFont(35, Font.BOLD));
+        this.setBounds(0, 0, 550, 300);
         setLayout(new BorderLayout());
         this.setIconImages(Arrays.asList(new ImageIcon(getClass().getResource("/Imagenes/logo-socosani.png")).getImage()));
         this.setLocationRelativeTo(null);
+
+        // Cargar las fuentes
+        try {
+            playwriteFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/Fuentes/PLAYWRITEITMODERNA-REGULAR.ttf")).deriveFont(18f);
+            robotoFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/Fuentes/Roboto-Regular.ttf")).deriveFont(18f);
+            aptosblack = Font.createFont(Font.TRUETYPE_FONT, new File("src/Fuentes/Aptos-Black.ttf")).deriveFont(18f);
+        } catch (FontFormatException | IOException e) {
+            e.printStackTrace();
+        }
+
         ComponentesVistaLogin();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
@@ -49,57 +64,54 @@ public class VistaLogin extends JFrame {
         table.setOpaque(false);
         ((DefaultTableCellRenderer) table.getDefaultRenderer(Object.class)).setOpaque(false);
 
-
-        
-
         // Ingresar los Datos
-        ControladorInterfaz.agregarEtiqueta(PanelVistaLogin, "USUARIO:", new Font("Roboto", Font.BOLD, 24), 25, 5, 200, 30);
-        iDUsuarioField = ControladorInterfaz.agregarCampoTexto(PanelVistaLogin, new Font("Roboto", Font.PLAIN, 18), 25, 45, 300, 30);
-        ControladorInterfaz.agregarEtiqueta(PanelVistaLogin, "CONTRASEÑA:", new Font("Roboto", Font.BOLD, 24), 25, 100, 200, 30);
+        ControladorInterfaz.agregarEtiqueta(PanelVistaLogin, "USUARIO:", robotoFont.deriveFont(Font.BOLD, 24), 25, 5, 200, 30);
+        iDUsuarioField = ControladorInterfaz.agregarCampoTexto(PanelVistaLogin, robotoFont.deriveFont(Font.PLAIN, 18), 25, 45, 300, 30);
+        ControladorInterfaz.agregarEtiqueta(PanelVistaLogin, "CONTRASEÑA:", robotoFont.deriveFont(Font.BOLD, 24), 25, 100, 200, 30);
         contrasenaField = new JPasswordField();
-        contrasenaField.setFont(new Font("Roboto", Font.PLAIN, 18));
+        contrasenaField.setFont(robotoFont.deriveFont(Font.PLAIN, 18));
         contrasenaField.setBounds(25, 135, 300, 30);
         PanelVistaLogin.add(contrasenaField);
 
         // Botones
-        
-        JButton btnIngresar = new JButton("Ingresar");
-        btnIngresar.setFont(new Font("PlaywriteITModerna", Font.BOLD, 20));
-        btnIngresar.setBounds(370, 35,160, 35);
-        PanelVistaLogin.add(btnIngresar);                                                                     
-        
-        JButton btnSalir = new JButton("Salir");
-        btnSalir.setFont(new Font("PlaywriteITModerna", Font.BOLD, 20));
-        btnSalir.setBounds(370, 120,160, 35);
-        PanelVistaLogin.add(btnSalir);                                                                     
-                                                                            
-        JButton btnContacto = new JButton("Contactanos");
-        btnContacto.setFont(new Font("PlaywriteITModerna", Font.BOLD, 20));
+        btnIngresar = new JButton("Ingresar");
+        btnIngresar.setFont(robotoFont.deriveFont(Font.BOLD, 20));
+        btnIngresar.setBounds(370, 35, 160, 35);
+        PanelVistaLogin.add(btnIngresar);
+
+        btnSalir = new JButton("Salir");
+        btnSalir.setFont(robotoFont.deriveFont(Font.BOLD, 20));
+        btnSalir.setBounds(370, 120, 160, 35);
+        PanelVistaLogin.add(btnSalir);
+
+        btnContacto = new JButton("Contactanos");
+        btnContacto.setFont(robotoFont.deriveFont(Font.BOLD, 20));
         btnContacto.setBounds(370, 250, 160, 35);
         PanelVistaLogin.add(btnContacto);
-
 
         // Añadir ActionListener para el botón registrar
         btnIngresar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                // Acción para el botón Ingresar
             }
         });
 
-        // ActionListener para el botón "Buscar"
-        btnIngresar.addActionListener(new ActionListener() {
+        // ActionListener para el botón Salir
+        btnSalir.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                // Acción para el botón Salir
             }
         });
-        
+
+        // ActionListener para el botón Contacto
         btnContacto.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                // Acción para el botón Contacto
             }
         });
     }
+
 }
